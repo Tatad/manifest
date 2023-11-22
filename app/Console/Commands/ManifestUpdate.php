685c6e1729b58900eece1a8f0c51c23a7c6f9cdc
Ryan David <ryan@tryus.ca>
2023-11-22 10:03:59 +0800

    updates on manifest update

--- app/Console/Commands/ManifestUpdate.php
+++ app/Console/Commands/ManifestUpdate.php
@@ -29,9 +29,9 @@ class ManifestUpdate extends Command
     {
         ini_set('max_execution_time', 2380);
         // get cURL resource
-        //$ch = curl_init();
+        $ch = curl_init();
         //$ids = [1660896, 2127647, 1596898,1691718 ,1635441, 2349196, 1891815, 1586356, 619899, 1640734, 2622054, 1627201, 845993, 3272378, 3272379];
-        $ids = Manifest::where('item_name', '=', null)->take(20)->get()->groupBy('item');
+        $ids = Manifest::where('item_name', '=', null)->take(1)->get()->groupBy('item');
         
         //dd($ids);
         //$ids = [1660896];
@@ -51,39 +51,35 @@ class ManifestUpdate extends Command
                 }
             }');
 
-            $client = new \GuzzleHttp\Client();
-            $request = $client->get('https://app.scrapingbee.com/api/v1/?api_key=ACZW8WWZ3SNFB1DYK7RD31I381GEDUXEGHMRBQZC1X90CVLO5H8X2KAT4PI92I1W8TNDAUKCINXLTHHB&url=https://www.costco.ca/CatalogSearch?keyword='.$key.'&extract_rules=' . $extract_rules);// Url of your choosing
-            $result = $request->getBody();
-            $res = json_decode($result->getContents(), true);
-            //dd($content);
-            // curl_setopt($ch, CURLOPT_URL, 'https://app.scrapingbee.com/api/v1/?api_key=ACZW8WWZ3SNFB1DYK7RD31I381GEDUXEGHMRBQZC1X90CVLO5H8X2KAT4PI92I1W8TNDAUKCINXLTHHB&url=https://www.costco.ca/CatalogSearch?keyword='.$key.'&extract_rules=' . $extract_rules);
+            curl_setopt($ch, CURLOPT_URL, 'https://app.scrapingbee.com/api/v1/?api_key=ACZW8WWZ3SNFB1DYK7RD31I381GEDUXEGHMRBQZC1X90CVLO5H8X2KAT4PI92I1W8TNDAUKCINXLTHHB&url=https://www.costco.ca/CatalogSearch?keyword='.$key.'&extract_rules=' . $extract_rules);
 
-            // // set method
-            // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            // set method
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
-            // // return the transfer as a string
-            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            // return the transfer as a string
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 
 
-            // // send the request and save response to $response
-            // $response = curl_exec($ch);
+            // send the request and save response to $response
+            $response = curl_exec($ch);
 
-            // // stop if fails
-            // if (!$response) {
-            //   die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
-            // }
+            // stop if fails
+            if (!$response) {
+              die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+            }
 
-            // echo 'HTTP Status Code: ' . curl_getinfo($ch, CURLINFO_HTTP_CODE) . PHP_EOL;
-            // echo 'Response Body: ' . $response . PHP_EOL;
-            // $res = json_decode($response,true);
+            echo 'HTTP Status Code: ' . curl_getinfo($ch, CURLINFO_HTTP_CODE) . PHP_EOL;
+            echo 'Response Body: ' . $response . PHP_EOL;
+            $res = json_decode($response,true);
             if($res['title'] != ""){
                 Manifest::where('item', $key)->update(['description' => $res['title'],'item_name' => json_encode($res['title']), 'images' => json_encode([$res['images']['src']])]);
             }else{
                 Manifest::where('item', $key)->update(['item_name' => 'not_available', 'images' => json_encode(['not_available'])]);
             }
-            // $results[] = $res;
+            $results[] = $res;
             // close curl resource to free up system resources
         }
+        curl_close($ch);
     }
 }
